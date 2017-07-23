7c162b064a4a0579824cef4d9a7ee0bd09dff33b
Ibrokhim <ibrohim95.tashkent@gmail.com>
2017-07-23 14:48:27 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, resp, next)=>{
+  var now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log +  '\n', (err)=>{
+    if(err){
+      console.log(err);
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next)=>{
+//   res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname+ '/public'));
+
+
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('ScreamIt', (text)=>{
+  return text.toUpperCase();
+});
+
+app.get('/', (req, resp)=>{
+  // resp.send('<h1 style="color:red;">Hello Ibrokhim!</h1>');
+  resp.render('Home.hbs', {
+    pageTitle: 'Home Page',
+    owner: 'Ibrokhim',
+  });
+});
+
+app.get('/about', (req, resp)=>{
+   resp.render('about.hbs', {
+     pageTitle: 'About Page',
+   });
+});
+
+app.get('/bad', (req, resp)=>{
+  resp.send({
+    error:'Bad request',
+    code:404,
+    message: 'No such page found',
+    furtherActions: 'Type page name correctly and refresh your browser'
+  });
+});
+app.listen(3000, ()=>{
+  console.log('Server is up on port 3000');
+});
